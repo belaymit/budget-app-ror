95f00714ac88ce1d9ecb10364f29d9038651eafe
belaymit <2belamit@gmail.com>
2023-04-27 21:31:08 +0300

    Modified font-family, color and background-color

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['proxima-nova', ...defaultTheme.fontFamily.sans],
       },
     },
   },
