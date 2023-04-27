944d0b6982ba70ab5d193bb7cbcd67d7aeaf86ea
belaymit <2belamit@gmail.com>
2023-04-27 13:46:22 +0300

    Reconfigured tailwindcss

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module BudgetAppRor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.schema_format = :ruby
   end
 end
